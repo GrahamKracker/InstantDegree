436b5a51f45ae240a4cb655960c60721707e5054
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2023-01-14 13:24:23 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,6 +6,6 @@ public static class ModHelperData
     public const string Name = "Instant Degree";
     public const string Description = "Adds a button to set the degree of a paragon within its tower selection menu, fixed by GrahamKraker (with permission), originally made by DepletedNova";
     public const string Version = "2.5.0";
-    public const string RepoOwner = "GrahamKraker";
+    public const string RepoOwner = "GrahamKracker";
     public const string RepoName = "InstantDegree";
 }
